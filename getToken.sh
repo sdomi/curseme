f931809f7658cf7d36dfd0fa345f80e7ecb9b37a
Dominika Liberda <ja@sdomi.pl>
2022-05-19 05:47:27 +0200

    + token stealer

---
+++ getToken.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [[ -f token ]]; then
+	echo "The token file already exists. Skipping..."
+	exit 0
+fi
+
+mkdir -p wrk; cd wrk
+
+wget https://curseforge.overwolf.com/downloads/curseforge-latest-linux.zip
+7z -y x curseforge-latest-linux.zip
+7z -y x *.AppImage
+grep -Poh 'cfCoreApiKey":".*?"' resources/app/dist/desktop/desktop.js | sed 's/.*://;s/"//g' > ../token
+
+cd ..; rm -R wrk
