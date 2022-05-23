bc5f5e95b3cd6c045a15248f77527c82c1b66c9a
Dominika Liberda <ja@sdomi.pl>
2022-05-23 12:20:32 +0200

    + fetching assets

--- launcher/version.sh
+++ launcher/version.sh
@@ -21,18 +21,34 @@ function fetch_version() {
 		return 1
 	fi
 
-	mkdir meow; cd meow
+	mkdir -p meow; cd meow
 	curl -O "$url"
 
 	mew="$(jq -r '.libraries[].downloads | if .classifiers then select(.classifiers["natives-linux"]) | .classifiers["natives-linux"] else .artifact end | "\(.url) \(.path)"' < $(basename "$url"))"
 
 	IFS=$'\n'
+	mkdir -p libraries; cd libraries
 	for i in $mew; do
-		url=$(awk '{print $1}' <<< "$i")
+		_url=$(awk '{print $1}' <<< "$i")
 		path=$(awk '{print $2}' <<< "$i")
 		mkdir -p $(dirname "$path")
-		curl -o "$path" "$url"
+		curl -o "$path" "$_url"
 	done
+	cd ..
+	curl -o client.jar "$(jq -r '.downloads.client.url' < "$(basename $url)")"
+	curl -o assets.json "$(jq -r '.assetIndex.url' < "$(basename $url)")"
+	
+	# assets
+	mkdir -p assets; cd assets
+	for i in $(jq -r '.objects | keys[] as $k | "\(.[$k].hash) \($k)"' < ../assets.json); do
+		hash=$(awk '{print $1}' <<< "$i")
+		hash_start=$(cut -c 1-2 <<< "$hash")
+		path=$(awk '{print $2}' <<< "$i")
+		mkdir -p $(dirname "$path")
+		curl -o $path "http://resources.download.minecraft.net/$hash_start/$hash"
+	done
+	cd ..
 	
 	cd ..
+	unset IFS
 }
